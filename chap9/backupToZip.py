dc4cc6de6dad5f248b721eb566e224722673752e
davidV <vdv.david@gmail.com>
2018-09-21 12:11:16 +0200

    manip files

---
+++ chap9/backupToZip.py
@@ -0,0 +1,27 @@
+#! python3
+# backupZi - Copies an entire folder and its content into a Zip file whose filename increments
+
+import zipfile, os
+
+def backupToZip(folder):
+	# Backup entire content of the 'folder' into a zip file.
+	folder = os.path.abspath(folder) # make sure folder is absolute
+	
+	# figure out the filename this code should use base on what files already exist
+	number = 1
+	while True:
+		zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+		if not os.path.exists(zipFilename)
+			break
+		number = number + 1
+
+	# Create the Zip file
+	print('Creating %s...' % (zipFilename))
+	backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+	# Walk the entire folder tree and compress the files in each folder
+	for foldername, subfolders, filenames in os.walk(folder):
+		print("Adding files in %s... " % (foldername))
+		# Add the current folder to the ZIP file.
+		backupZip.write(foldername)
+		# Add all the files in this folder to the Zip file.
---
+++ chap9/renameDates.py
@@ -0,0 +1,42 @@
+#! python3
+# renamesDates.py - Renames filenames with American MM-DD-YYYY date format to European DD-MM-YYYY.
+
+import shutil, os, re
+
+# Create regex that matches files with the American date format
+datePattern = re.compile(r"""^(.*?)
+	((0|1)?\d)-
+	((0|1|2|3)?\d)-
+	((19|20)\d\d)
+	(.*?)$
+	""", re.VERBOSE)
+
+# Global batch numbering of files that matches
+index = 1
+
+# Loop over the files in the working directory
+for amerFilename in os.listdir('/media/davidv/HD_SYMENT/'):
+	mo = datePattern.search(amerFilename)
+
+	# Skip files without a date
+	if mo == None:
+		continue
+
+	beforePart = mo.group(1)
+	monthPart = mo.group(2)
+	dayPart = mo.group(4)
+	yearPart = mo.group(6)
+	afterPart = mo.group(8)
+
+	# Form the European-style filename
+	euroFilename = str(index) + '__' + beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+	index = index + 1
+
+	# Get the full, absolute file paths.
+	absWorkingDir = os.path.abspath('/media/davidv/HD_SYMENT/')
+	amerFilename = os.path.join(absWorkingDir, amerFilename)
+	euroFilename = os.path.join(absWorkingDir, euroFilename)
+
+	# Rename the files
+	print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+	# shutil.move(amerFilename, euroFilename) # uncomment after testing
